36a66a125e6ffaa7e8dfd57b40acf6e2b8ccf371
CharbelFarah057 <charbel0farah@gmail.com>
2023-03-28 00:09:14 +0300

    Login Page frontend done

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-const LoginPage = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Add logic for authentication here
+function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!username || !password) {
+      setErrorMessage('Please enter your username and password.');
+      return;
+    }
+
+    // Implement actual authentication logic here
+    setErrorMessage('');
   };
 
   return (
-    <div className="container">
-      <h1>Linux Firewall</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          required
-        />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
-};
-
-function App() {
-  return (
     <div className="App">
-      <LoginPage />
+      <div className="container">
+        <h1>Linux Firewall</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {/* Insert error-message-container here */}
+          <div className="error-message-container">
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
+          </div>
+          <button type="submit">Log In</button>
+        </form>
+      </div>
     </div>
   );
 }
